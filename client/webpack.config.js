4368df1f28ba852b633f05a3ae8b3bb351a70afb
clarkera <rclarke9941@gmail.com>
2024-08-08 19:25:24 -0400

    webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+module.exports = {
+  // ... other configurations ...
+  devServer: {
+    allowedHosts: [
+      "https://taskit-qpct.onrender.com", // Replace with your Render domain
+    ],
+  },
+};
