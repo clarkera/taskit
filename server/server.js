2a4b991d99f1bf35a1c390462451094de10e85c9
clarkera <rclarke9941@gmail.com>
2024-08-08 21:27:54 -0400

    null

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dataBase = require("./config/connection");
 const routes = require("./routes");
 const cors = require("cors");
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
