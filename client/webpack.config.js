c83dc9ba19ce93ac3bb0223b2bb9944a56da9a08
clarkera <rclarke9941@gmail.com>
2024-08-08 20:10:23 -0400

    removed modules

--- client/webpack.config.js
+++
@@ -1,9 +0,0 @@
-// webpack.config.js
-module.exports = {
-  // ... other configurations ...
-  devServer: {
-    allowedHosts: [
-      "https://taskit-qpct.onrender.com", // Replace with your Render domain
-    ],
-  },
-};
